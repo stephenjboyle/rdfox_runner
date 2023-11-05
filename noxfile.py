89079f9024f1bc569d8a1d2a19188f1b504eb5c4
Rick Lupton <mail@ricklupton.name>
2023-11-05 16:51:54 +0000

    Actions: test with nox (multiple RDFox versions)

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ RDFOX_VERSIONS_IDS = [f"rdfox{version.replace('.', '')}" for version in RDFOX_VE
 @nox.session
 @nox.parametrize("rdfox", RDFOX_VERSIONS, ids=RDFOX_VERSIONS_IDS)
 def tests(session, rdfox):
-    session.install("pytest~=7")
+    session.install("pytest~=7.0")
     session.install(f"rdfox=={rdfox}")
     session.install("-e", ".")
     session.run("pytest", "tests", "--log-level=DEBUG", *session.posargs)
